6167b1457df841952d9325fc0f6dbff0f89812fb
xdiegors <xdiegors@outlook.com>
2022-07-13 15:40:53 -0300

    Rotas ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Erro404Component } from './components/erro404/erro404.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contato', component: ContatoComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: '**', component: Erro404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { Erro404Component } from './components/erro404/erro404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContatoComponent,
+    InicioComponent,
+    SobreComponent,
+    Erro404Component
   ],
   imports: [
     BrowserModule,
